a614819d3763a9f4887f4373e4b2f39f43640d1e
hxy59494 <1147102554@qq.com>
2021-04-10 21:59:34 +0800

    修改了基本路径

--- src/main.js
+++ src/main.js
@@ -8,8 +8,8 @@ import './assets/css/global.css'
 import axios from 'axios'
 //配置请求的跟路径
 // axios.defaults.baseURL = "http://timemeetyou.com:8889/api/private/v1/"
-axios.defaults.baseURL = "http://localhost:8081/"
-// axios.defaults.baseURL = "http://42.194.196.25:8081/"
+// axios.defaults.baseURL = "http://localhost:8081/"
+axios.defaults.baseURL = "http://42.194.196.25:8081/"
 axios.interceptors.request.use(config => {
   config.headers.Authorization = window.sessionStorage.getItem('token')
   return config
